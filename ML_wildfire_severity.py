91bfebb9f7092196a0c524229418bdb8f381403f
cfinkenbiner <c.finkenbiner@gmail.com>
2021-08-27 17:48:46 -0400

    Add files via upload

---
+++ ML_wildfire_severity.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+import cartopy
+import cartopy.crs as ccrs
+import matplotlib as mpl
+from mpl_toolkits.axes_grid1 import AxesGrid
+from cartopy.mpl.geoaxes import GeoAxes
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+
+def ML_model(state):
+
+    df_state = pd.read_csv('WildFires_'+state+'.csv', index_col=False, low_memory=False)
+    
+    ## Missing Values
+    #df_state.isna().sum()
+    
+    ## Split into training & testing datasets
+    train_data, test_data = train_test_split(df_state, test_size=0.2, random_state=25)
+
+    print(f"Number of training examples: {train_data.shape[0]}")
+    print(f"Number of testing examples: {test_data.shape[0]}")
+    
+    y_train = train_data['FIRE_SIZE']
+    y_test = test_data['FIRE_SIZE']
+
+    X_train = train_data.drop(['FIRE_SIZE', 'FIRE_SIZE_CLASS'], axis=1)
+    x_test = test_data.drop(['FIRE_SIZE', 'FIRE_SIZE_CLASS'], axis=1)
+    
+    # Selected string columns with no missing values
+    categorical_columns = ['FPA_ID', 'SOURCE_SYSTEM_TYPE', 'SOURCE_SYSTEM', 'NWCG_REPORTING_AGENCY', 
+                           'NWCG_REPORTING_UNIT_ID', 'NWCG_REPORTING_UNIT_NAME', 'SOURCE_REPORTING_UNIT_NAME',
+                           'OWNER_DESCR', 'STAT_CAUSE_DESCR', 'STATE']
+
+    scale_columns = ['FOD_ID', 'FIRE_YEAR', 'DISCOVERY_DATE', 'DISCOVERY_DOY', 'STAT_CAUSE_CODE', 'OWNER_CODE']
+
+    numeric_columns = ['LATITUDE', 'LONGITUDE', 'VEG_TYPE', 'SOIL_TYPE']
+
+    features = ColumnTransformer([
+        ('categorical', OneHotEncoder(handle_unknown = 'ignore'), categorical_columns),
+        ('scaler', StandardScaler(), scale_columns),
+        ('numeric', 'passthrough', numeric_columns)
+        ])
+
+    est = Pipeline([
+        ('features', features),
+        ('estimator', LinearRegression())
+        ])
+    
+    # Estimator
+    est.fit(X_train, y_train)
+    
+    return x_test, y_test, est.predict(x_test)
+
+# For plotting predicted fire acres into classes
+def fire_num_calc(acres):
+    output = []
+    for a in acres:
+        if a < 0.25:
+            output.append(1)
+        elif 0.25 <= a < 10:
+            output.append(2)  
+        elif 10 <= a < 100:
+            output.append(3)   
+        elif 100 <= a < 300:
+            output.append(4)  
+        elif 300 <= a < 1000:
+            output.append(5)  
+        elif 1000 <= a < 5000:
+            output.append(6) 
+        elif a > 5000:
+            output.append(7)
+        else:
+            output.append(None)
+                
+    return np.array(output)
+
+# Translates prediction into fire classes
+def fire_class_calc(acres):
+    output = []
+    for a in acres:
+        if a < 0.25:
+            output.append('A')
+        elif 0.25 <= a < 10:
+            output.append('B')  
+        elif 10 <= a < 100:
+            output.append('C')   
+        elif 100 <= a < 300:
+            output.append('D')  
+        elif 300 <= a < 1000:
+            output.append('E')  
+        elif 1000 <= a < 5000:
+            output.append('F') 
+        elif a > 5000:
+            output.append('G')
+        else:
+            output.append('NA')
+                
+    return np.array(output)
+
+
+def plot_predictions(state, x_test, y_test, y_pred):
+    if state == 'CA':
+        lat_lon = [-125, -113, 30, 43] 
+    if state == 'OR':    
+        lat_lon = [-125, -115, 41, 47] 
+    if state == 'WA':    
+        lat_lon = [-126, -116.5, 45, 50] 
+    if state == 'NV':    
+        lat_lon = [-121, -113, 34, 43] 
+
+    def create_map(lat, lon, values, label, current_subplot, ii):
+        ax = current_subplot
+        ax.set_extent(lat_lon, ccrs.Geodetic())
+        gl = ax.gridlines(linestyle='--', draw_labels=True)
+        gl.xlabels_top = False
+        gl.ylabels_right = False
+        ax.coastlines()
+
+        im = ax.scatter(lon, lat, 
+                        alpha=0.85, 
+                        s=8,
+                        c = values,
+                        cmap= plt.cm.get_cmap('plasma', 7),
+                        transform = ccrs.PlateCarree())
+
+        cbar = axgr.cbar_axes[ii].colorbar(im)
+        cbar.set_ticks([1, 2, 3, 4, 5, 6, 7])
+        cbar.set_ticklabels(["A", "B", "C", "D", "E", "F", "G"])
+        axgr[ii].set_title(label)
+
+    projection = ccrs.PlateCarree()
+    axes_class = (GeoAxes, dict(map_projection = projection))   
+
+    fig = plt.figure(1,figsize = (12,10))
+    axgr = AxesGrid(fig, 111, axes_class = axes_class, 
+                        nrows_ncols=(1,2), 
+                        axes_pad=0.90,
+                        share_all = True,
+                        cbar_location = 'right',
+                        cbar_mode = 'edge',
+                        direction = 'row',
+                        cbar_pad = 0.1,
+                        cbar_size = '3%',
+                        label_mode = '')
+
+    create_map(x_test['LATITUDE'].values, x_test['LONGITUDE'].values,
+               fire_num_calc(y_test),
+               'Historical Recorded Fire Class', 
+               axgr[0], 0)
+
+    create_map(x_test['LATITUDE'].values, x_test['LONGITUDE'].values,
+               fire_num_calc(y_pred),
+               'ML Predicted Fire Class', 
+               axgr[1], 1)   
+
+def make_predictions(state):
+    
+    # test dataset and prediction on test
+    x_test, y_test, y_pred = ML_model(state)
+    
+    # Make figure
+    print('\n Fire Class Records from and Predictions on the test dataset:')
+    plot_predictions(state, x_test, y_test, y_pred)
+    
+if __name__ == '__main__':
+    make_predictions(state)
---
+++ data_visualizations.py
@@ -0,0 +1,184 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+
+import cartopy
+import cartopy.crs as ccrs
+import matplotlib as mpl
+from mpl_toolkits.axes_grid1 import AxesGrid
+from cartopy.mpl.geoaxes import GeoAxes
+
+## Calculate & Plot Number of Fires per Year
+def fig_fire_yr(df, start_yr, end_yr):
+
+    years = np.arange(start_yr, end_yr) # date range
+
+    num_fires = []
+    for yr in years:
+        df_yr = df[df['FIRE_YEAR'] == yr]
+        num_fires.append(df_yr['OBJECTID'].nunique())
+    num_fires = np.array(num_fires)
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(years, num_fires)
+
+    plt.figure()
+    plt.plot(years, num_fires, 'r^-')
+    plt.plot(years, years*slope+intercept, 'k-')
+
+    plt.title('Total Reported Fires in the U.S. by Year')
+    plt.ylabel('Number of Reported Fires')
+
+    print(r'Total Reported Fires:')
+    print(r' r2 = %.3f' % r_value**2)
+    print(r' p-value = %.3f' % p_value)
+
+if __name__ == '__main__':
+    fig_fire_yr(df, start_yr, end_yr)
+
+    
+## Calculate & Plot Number of Fires for Each State
+def fig_fire_by_state(df, start_yr, end_yr):
+    states = df['STATE'].unique()
+    num_fires = []
+    for s in states:
+        df_s = df[df['STATE'] == s]
+        num_fires.append(df_s['OBJECTID'].nunique())
+    num_fires = np.array(num_fires)
+
+    plt.figure(figsize=(12,3))
+    plt.bar(states, num_fires, color='orange')
+
+    plt.xticks(rotation = 90)
+    plt.title('Total Reported Fires by State (' +str(start_yr)+' - ' +str(end_yr)+ ')')
+    plt.ylabel('Number of Reported Fires')
+    
+if __name__ == '__main__':
+    fig_fire_by_state(df, start_yr, end_yr)
+    
+
+## Calculate Acres within the Final Perimeter of the Fire
+def fig_acres_final(df, start_yr, end_yr):
+    years = np.arange(start_yr, end_yr) # date range
+
+    fire_mean = []
+    fire_max = []
+    for yr in years:
+        df_yr = df[df['FIRE_YEAR'] == yr]
+        fire_mean.append(df_yr['FIRE_SIZE'].mean())
+        fire_max.append(df_yr['FIRE_SIZE'].max())
+    fire_mean = np.array(fire_mean)
+    fire_max = np.array(fire_max)
+
+    plt.figure(figsize=(10,4))
+    plt.subplot(1,2,1)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(years, fire_mean)
+    plt.plot(years, fire_mean, 'D-', color='purple')
+    plt.plot(years, years*slope+intercept, 'k-')
+
+    print("Average Number of Acres:")
+    print(r' r2 = %.3f' % r_value**2)
+    print(r' p-value = %.3f' % p_value)
+
+    plt.title('Acres within the Final Perimeter of the Fire')
+    plt.ylabel('Average Number of Acres')
+
+    plt.subplot(1,2,2)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(years, fire_max)
+    plt.plot(years, fire_max, 'gD-')
+    plt.plot(years, years*slope+intercept, 'k-')
+
+    print("Maximum Number of Acres:")
+    print(r' r2 = %.3f' % r_value**2)
+    print(r' p-value = %.3f' % p_value)
+
+    plt.title('Acres within the Final Perimeter of the Fire')
+    plt.ylabel('Maximum Number of Acres')
+    plt.tight_layout()
+    
+if __name__ == '__main__':
+    fig_acres_final(df, start_yr, end_yr)
+    
+
+## Calculate Number of Fires in Each Class Size for a State
+def fig_fire_class(df, start_yr, end_yr, state):
+    years = np.arange(start_yr, end_yr) # date range
+
+    df_state = df[df['STATE'] == state]
+
+    df_state['FIRE_SIZE_CLASS'].value_counts().plot(kind='bar')
+    plt.title('Number of Fires in Each Size Class for '+state)
+    plt.xlabel('Fire Size Class')
+    plt.ylabel('Number of Fires')
+    
+if __name__ == '__main__':
+    fig_fire_class(df, start_yr, end_yr, state)
+
+    
+def fig_soil_veg_map(state):
+    # load appropriate datafile
+    df_state = pd.read_csv('WildFires_'+state+'.csv', index_col=False, low_memory=False)
+    
+    if state == 'CA':
+        lat_lon = [-125, -113, 30, 43] 
+    if state == 'OR':    
+        lat_lon = [-125, -115, 41, 47] 
+    if state == 'WA':    
+        lat_lon = [-126, -116.5, 45, 50] 
+    if state == 'NV':    
+        lat_lon = [-121, -113, 34, 43] 
+        
+    def create_map(lat, lon, values, label, current_subplot, ii):
+        ax = current_subplot
+        ax.set_extent(lat_lon, ccrs.Geodetic())
+        gl = ax.gridlines(linestyle='--', draw_labels=True)
+        gl.xlabels_top = False
+        gl.ylabels_right = False
+        ax.coastlines()
+
+        cmap = plt.cm.tab20b  # define the colormap
+        # extract all colors from the .jet map
+        cmaplist = [cmap(i) for i in range(cmap.N)]
+
+        # define the bins and normalize
+        bounds = np.unique(values)
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+        # create the new map
+        cmap = mpl.colors.LinearSegmentedColormap.from_list('Custom cmap', cmaplist, cmap.N)
+
+        im = ax.scatter(lon, lat, 
+                        alpha=0.85, 
+                        s=8,
+                        c = values,
+                        cmap = cmap,
+                        norm = norm,
+                        transform = ccrs.PlateCarree())
+        axgr.cbar_axes[ii].colorbar(im)
+        axgr[ii].set_title(label)
+
+    projection = ccrs.PlateCarree()
+    axes_class = (GeoAxes, dict(map_projection = projection))   
+
+    fig = plt.figure(1,figsize = (12,10))
+    axgr = AxesGrid(fig, 111, axes_class = axes_class, 
+                        nrows_ncols=(1,2), 
+                        axes_pad=0.90,
+                        cbar_location = 'right',
+                        cbar_mode = 'each',
+                        cbar_pad = 0.1,
+                        cbar_size = '3%',
+                        label_mode = '')
+
+    create_map(df_state['LATITUDE'].values, df_state['LONGITUDE'].values,
+               df_state['SOIL_TYPE'].values,
+               'Soil Texture Classification', 
+               axgr[0], 0)
+
+    create_map(df_state['LATITUDE'].values, df_state['LONGITUDE'].values,
+               df_state['VEG_TYPE'].values,
+               'Vegetation Classification', 
+               axgr[1], 1)   
+
+if __name__ == '__main__':
+    fig_soil_veg_map(state)
