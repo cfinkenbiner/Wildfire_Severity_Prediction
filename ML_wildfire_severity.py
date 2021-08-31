67a850fb1841cbb0d343e7515544c12736e158ac
cfinkenbiner <c.finkenbiner@gmail.com>
2021-08-30 23:10:15 -0400

    Add files via upload

--- ML_wildfire_severity.py
+++ ML_wildfire_severity.py
@@ -10,13 +10,16 @@ from mpl_toolkits.axes_grid1 import AxesGrid
 from cartopy.mpl.geoaxes import GeoAxes
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
 
 
 def ML_model(state):
@@ -32,8 +35,8 @@ def ML_model(state):
     print(f"Number of training examples: {train_data.shape[0]}")
     print(f"Number of testing examples: {test_data.shape[0]}")
     
-    y_train = train_data['FIRE_SIZE']
-    y_test = test_data['FIRE_SIZE']
+    y_train = train_data['FIRE_SIZE_CLASS']
+    y_test = test_data['FIRE_SIZE_CLASS']
 
     X_train = train_data.drop(['FIRE_SIZE', 'FIRE_SIZE_CLASS'], axis=1)
     x_test = test_data.drop(['FIRE_SIZE', 'FIRE_SIZE_CLASS'], axis=1)
@@ -43,48 +46,55 @@ def ML_model(state):
                            'NWCG_REPORTING_UNIT_ID', 'NWCG_REPORTING_UNIT_NAME', 'SOURCE_REPORTING_UNIT_NAME',
                            'OWNER_DESCR', 'STAT_CAUSE_DESCR', 'STATE', 'LATITUDE', 'LONGITUDE']
 
-    scale_columns = ['FOD_ID', 'FIRE_YEAR', 'DISCOVERY_DATE', 'DISCOVERY_DOY', 'STAT_CAUSE_CODE', 'OWNER_CODE']
+    scale_columns = ['FOD_ID', 'FIRE_YEAR', 'DISCOVERY_DATE', 'DISCOVERY_DOY', 'STAT_CAUSE_CODE', 'OWNER_CODE', 
+                    'VEG_TYPE', 'SOIL_TYPE']
 
     numeric_columns = ['VEG_TYPE', 'SOIL_TYPE']
 
     features = ColumnTransformer([
-        ('categorical', OneHotEncoder(handle_unknown = 'ignore'), categorical_columns),
+        #('categorical', OneHotEncoder(handle_unknown = 'ignore'), categorical_columns),
         ('scaler', StandardScaler(), scale_columns),
         ('numeric', 'passthrough', numeric_columns)
         ])
 
     est = Pipeline([
         ('features', features),
-        ('estimator', LinearRegression())
+        ('estimator', RandomForestClassifier(n_estimators = 100, 
+                                             max_depth = 25, 
+                                             warm_start = True))
         ])
     
     # Estimator
     est.fit(X_train, y_train)
+    # Predictions
+    y_pred = est.predict(x_test)
     
-    return x_test, y_test, est.predict(x_test)
+    return x_test, y_test, y_pred
+
 
 # For plotting predicted fire acres into classes
 def fire_num_calc(acres):
     output = []
     for a in acres:
-        if a < 0.25:
+        if a == 'A':
             output.append(1)
-        elif 0.25 <= a < 10:
+        elif a == 'B':
             output.append(2)  
-        elif 10 <= a < 100:
+        elif a == 'C':
             output.append(3)   
-        elif 100 <= a < 300:
+        elif a == 'D':
             output.append(4)  
-        elif 300 <= a < 1000:
+        elif a == 'E':
             output.append(5)  
-        elif 1000 <= a < 5000:
+        elif a == 'F':
             output.append(6) 
-        elif a > 5000:
+        elif a == 'G':
             output.append(7)
         else:
-            output.append(None)
-                
+            output.append(1)
+
     return np.array(output)
+
 
 # Translates prediction into fire classes
 def fire_class_calc(acres):
@@ -105,7 +115,7 @@ def fire_class_calc(acres):
         elif a > 5000:
             output.append('G')
         else:
-            output.append('NA')
+            output.append('A')
                 
     return np.array(output)
 
@@ -164,15 +174,22 @@ def plot_predictions(state, x_test, y_test, y_pred):
                fire_num_calc(y_pred),
                'Predicted Fire Class', 
                axgr[1], 1)   
-
+    
+    
+def calc_model_stats(y_test, y_pred):
+    print('\n Accuracy: ', accuracy_score(y_test, y_pred))
+    
+    
 def make_predictions(state):
     
     # test dataset and prediction on test
     x_test, y_test, y_pred = ML_model(state)
     
     # Make figure
-    print('\n Fire Class Records from and Predictions on the test dataset:')
     plot_predictions(state, x_test, y_test, y_pred)
+    
+    # Calculate Statistics
+    calc_model_stats(y_test, y_pred)
     
 if __name__ == '__main__':
     make_predictions(state)
